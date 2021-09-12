45f7bb646256c0cf83c3a8fef2becf99b9f59f21
linkslayer <kshitij.singh998@gmail.com>
2021-09-12 14:17:35 +0530

    updated controller and added yaml file

--- src/main/java/com/hello/demo/controller/HelloController.java
+++ src/main/java/com/hello/demo/controller/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/hello-world")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "hello from spring docker container!";
 	}
 
 }
